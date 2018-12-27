1a03ba955aa2ad5e00fb83fe872aab69b77bafb0
urmi-21 <mrbains21@gmail.com>
2018-12-27 01:08:13 -0600

    Update plotAM.R

--- plotAM.R
+++ plotAM.R
@@ -3,6 +3,8 @@ BiocManager::install("affy", version = "3.8")
 library("ArrayExpress")
 library(affy)
 library(affydata)
+library(ggbiplot)
+memory.limit(size=56000)
 
 #code Ref: http://math.usu.edu/jrstevens/stat5570/2.4.Visualization.pdf
 data(Dilution)
@@ -14,6 +16,16 @@ A <- (Y + X) / 2
 plot(A,M,main= 'default M-A plot',pch= 16,cex = 0.1);abline(h = 0)
 
 pc <- prcomp(t(eset))
+
 names(pc)
 group <- as.numeric(colnames(eset))
 plot(pc$x[, 1], pc$x[, 2], main = "PCA", xlab = "PC1", ylab = "PC2",col=c(1,2,3,4))
+g <- ggbiplot(pc, scale = 1,obs.scale = 1, varname.abbrev = FALSE,var.axes = FALSE,pc.biplot =TRUE,circle = TRUE)
+g
+##using NOISeq
+myPCA<-PCA.GENES(t(eset))
+myPCA$var.exp
+explo.plot(myPCA)
+explo.plot(myPCA)
+
+myPCA = dat(eset, type = "PCA")
