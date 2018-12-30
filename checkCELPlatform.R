61a5b8f509083782000ec5f4112341aa204c3e82
Urminder Singh <mrbains21@gmail.com>
2018-12-29 19:01:11 -0600

    Update checkCELPlatform.R

--- checkCELPlatform.R
+++ checkCELPlatform.R
@@ -9,18 +9,13 @@ affy.data = ReadAffy(filenames = ATH1files)
 corrupted.files <- c()
 goodFiles<-c()
 for(i in 1:length(ATH1files)) {
-err <<- F
+    err <<- F
     x <- tryCatch(ReadAffy(filenames=ATH1files[i]), error = function(e){print(e); err <<- T}, finally=print(paste("finished with", ATH1files[i], "at", Sys.time())))
-
-        if(err){
-                corrupted.files <- c(corrupted.files,ATH1files[i])
-                 print(paste("curroptedFile", ATH1files[i]))
-
-        }
-else{
+    if(err){
+        corrupted.files <- c(corrupted.files,ATH1files[i])
+        print(paste("curroptedFile", ATH1files[i]))
+    }
+    else{
         goodFiles<-c(goodFiles,ATH1files[i])
         }
-
-        
-
 }
