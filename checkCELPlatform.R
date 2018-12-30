241b85a2c3f4b46d69502f37a2245889e34711aa
Urminder Singh <mrbains21@gmail.com>
2018-12-30 12:40:59 -0600

    Update checkCELPlatform.R

--- checkCELPlatform.R
+++ checkCELPlatform.R
@@ -1,7 +1,7 @@
 #checkPlatform in CEL files
 library(affyio)
 files = list.files(pattern = "\\.CEL$", full.names = TRUE,ignore.case=T)
-platforms<- table(sapply(files, function(x) read.celfile.header(x)$cdfName))
+#platforms<- table(sapply(files, function(x) read.celfile.header(x)$cdfName))
 filePlatform<-sapply(files, function(x) read.celfile.header(x)$cdfName)
 ATH1files<-names(filePlatform[which(filePlatform=="ATH1-121501")])
 #affy.data = ReadAffy(filenames = ATH1files)
