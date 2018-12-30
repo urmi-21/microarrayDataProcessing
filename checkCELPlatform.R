35569b8322c9dcd31c514bd4bc3ad747e7d6eddc
Urminder Singh <mrbains21@gmail.com>
2018-12-29 19:51:39 -0600

    Update checkCELPlatform.R

--- checkCELPlatform.R
+++ checkCELPlatform.R
@@ -4,7 +4,7 @@ files = list.files(pattern = "\\.CEL$", full.names = TRUE,ignore.case=T)
 platforms<- table(sapply(files, function(x) read.celfile.header(x)$cdfName))
 filePlatform<-sapply(files, function(x) read.celfile.header(x)$cdfName)
 ATH1files<-names(filePlatform[which(filePlatform=="ATH1-121501")])
-affy.data = ReadAffy(filenames = ATH1files)
+#affy.data = ReadAffy(filenames = ATH1files)
 
 corrupted.files <- c()
 goodFiles<-c()
@@ -19,3 +19,5 @@ for(i in 1:length(ATH1files)) {
         goodFiles<-c(goodFiles,ATH1files[i])
         }
 }
+
+affy.data = ReadAffy(filenames = goodFiles)
